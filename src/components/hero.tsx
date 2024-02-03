ecd62ce36b408a74151dec928d6a6f25109d3010
zaintellectual <zaintellectual@gmail.com>
2024-02-03 07:15:55 -0800

    update shadcnui

---
+++ src/components/hero.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { DialogDemo } from './dialog'
+
+const Hero = () => {
+  return (
+    <section className="text-gray-600 body-font">
+  <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+    <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+        Noman Bhai Ye Bhe Dkhein
+        <br className="hidden lg:inline-block" />
+        Mai ny Hero or Dialoge ko relate kiya
+      </h1>
+      <p className="mb-8 leading-relaxed">
+       Mera swaal ye hy k 1 component ko install krny
+       k baad hum new same name ka Q bnaty hyn?
+      </p>
+      <div className="flex justify-center">
+        <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+          Reply
+        </button>
+        <DialogDemo></DialogDemo>
+      </div>
+    </div>
+    <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+      {/* <img
+        className="object-cover object-center rounded"
+        alt="hero"
+        src="https://dummyimage.com/720x600"
+      /> */}
+    </div>
+  </div>
+</section>
+
+  )
+}
+
+export default Hero
